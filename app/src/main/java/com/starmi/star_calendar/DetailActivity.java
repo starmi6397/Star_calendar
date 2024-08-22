55bbaa600ce1067aed4939cc2cc364f1c37b996e
starmi <zhangzijian2222@outlook.com>
2024-08-22 23:29:37 +0800

    重写，并增加功能

--- app/src/main/java/com/starmi/star_calendar/DetailActivity.java
+++ app/src/main/java/com/starmi/star_calendar/DetailActivity.java
@@ -5,7 +5,6 @@ import android.util.Log;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
