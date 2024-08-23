b149ad1d6a34316efcf5e7b05f99b0f8c0cb3bd0
starmi <zhangzijian2222@outlook.com>
2024-08-23 21:45:41 +0800

    回滚部分代码

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,7 @@ dependencies {
     implementation(libs.play.services.wearable)
     // Material Design components
     //noinspection UseTomlInstead
-    implementation ("androidx.compose.material:material:1.6.8")
-    implementation ("androidx.appcompat:appcompat:1.3.0")
+    implementation("com.google.android.material:material:1.12.0")
     //Gson
     //noinspection UseTomlInstead
     implementation ("com.google.code.gson:gson:2.10.1")
